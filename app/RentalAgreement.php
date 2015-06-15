4a3b4da0f74a621720edbb87c74260b2c5b2a009
arian <arianpourinfo@gmail.com>
2015-06-16 03:00:09 +0800

    Added the $fillable to all Models as needed

--- app/RentalAgreement.php
+++ app/RentalAgreement.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class RentalAgreement extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = ['dateOfAgreement','commencingDate','expireDate'
+                            ,'rentalAmount','rentalDeposit','utilitiesDeposit'
+                            ,'otherDeposit','premiseUse'];
+
 }
